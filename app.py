3f9775981c001ebd237520765023e4a795cf56cc
MarinaVlasova <vlasovamarisha2004@gmail.com>
2024-12-22 23:40:13 +0700

    check build

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Jenkins! This is your app running in Docker."
+    return "AVS Top."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8081)
