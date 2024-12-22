5de66e39a2370c2723693ac8d0a72fe8fb8767af
MarinaVlasova <vlasovamarisha2004@gmail.com>
2024-12-22 21:52:40 +0700

    update changes

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(name)
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Hello, Jenkins! This is your app running in Docker."
 
-if name == 'main':
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8081)
